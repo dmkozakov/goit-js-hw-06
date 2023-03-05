6c8848627097911763dcf0c60aef612c9b18994d
Dmytro Kozakov <dimakozakov1999@gmail.com>
2023-03-05 18:20:30 +0200

    Add task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const list = document.querySelector("#ingredients");
+
+function createListFromArray(arr, parentEl) {
+  const elements = arr.map(el => {
+    const listItem = document.createElement("li");
+    listItem.textContent = el;
+    listItem.classList.add("item");
+    return listItem;
+  });
+
+  parentEl.append(...elements);
+}
+
+createListFromArray(ingredients, list);
